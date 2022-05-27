18f46a00935ecfa7c87636b678333a2b098f563d
Prajwal Adhikari <prajwalad101@gmail.com>
2022-05-27 21:03:32 +0545

    initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+// const express = require('express');
+
+// const PORT = process.env.PORT || 3001;
+
+// const app = express();
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening on ${PORT}`);
+// });
+
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
