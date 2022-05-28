cd896c68eb64725d3d493fbd570cd9f12aa20b4f
Prajwal Adhikari <prajwalad101@gmail.com>
2022-05-28 10:47:14 +0545

    connect to mongodb database

--- server/index.js
+++ server/index.js
@@ -1,21 +1,27 @@
-// const express = require('express');
-
-// const PORT = process.env.PORT || 3001;
-
-// const app = express();
-
-// app.listen(PORT, () => {
-//   console.log(`Server listening on ${PORT}`);
-// });
-
 const express = require('express');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+mongoose
+  .connect(
+    'mongodb+srv://prajwal:prajwalad101@cluster0.o1p1x.mongodb.net/portfolio?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('DB connection successful');
+  })
+  .catch((err) => console.log('Error while connecting to the database'));
+
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from server!' });
+});
+
+app.post('/api/stock', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+  });
 });
 
 app.listen(PORT, () => {
